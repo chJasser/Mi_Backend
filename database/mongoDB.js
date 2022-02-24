85f2256088ec4c218cd02ee8359a709a0aa95a67
Chaieb Jasser <chaieb.jasser@esprit.tn>
2022-02-24 14:20:41 +0100

    first commit

---
+++ database/mongoDB.js
@@ -0,0 +1,10 @@
+const dotenv = require("dotenv").config();
+const mongoose = require("mongoose");
+mongoose
+  .connect(process.env.DATA_BASE_URL)
+  .then(() => {
+    console.log("DataBase connected Successfully");
+  })
+  .catch(() => {
+    console.log("Error connecting to DataBase");
+  });
---
+++ models/user.js
@@ -0,0 +1,68 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Create Schema
+const UserSchema = new Schema({
+  cin: {
+    type: integer,
+    required: true,
+    unique: true,
+  },
+  userName: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  birthDate: {
+    type: Date,
+    required: true,
+  },
+  sexe: {
+    type: String,
+    enum: ["man", "women"],
+    required: true,
+  },
+
+  address: {
+    type: String,
+    required: true,
+  },
+  Zip_Code: {
+    type: String,
+    required: true,
+  },
+  profilePicture: {
+    type: String,
+    required: true,
+  },
+  phoneNmuber: {
+    type: String,
+    required: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+module.exports = User = mongoose.model("users", UserSchema);
